ed00ae26d8bc48a7e10aa21fa41f4ffe3b85452c
Sallee <885331635@qq.com>
2024-02-18 10:18:24 +0800

    添加项目文件。

---
+++ LibSimpleNetworKit/src/networkit/auxiliary/Multiprecision.hpp
@@ -0,0 +1,19 @@
+/*
+ * Multiprecision.hpp
+ *
+ *  Created on: 21.03.2013
+ *      Author: cls
+ */
+
+#ifndef NETWORKIT_AUXILIARY_MULTIPRECISION_HPP_
+#define NETWORKIT_AUXILIARY_MULTIPRECISION_HPP_
+
+ //Remove asm to ensure multi-platform compatibility
+#define TTMATH_NOASM  
+#include <ttmath/ttmath.h>
+
+namespace NetworKit {
+  using bigfloat = ttmath::Big<TTMATH_BITS(64), TTMATH_BITS(64)>; ///< big floating point number
+}
+
+#endif // NETWORKIT_AUXILIARY_MULTIPRECISION_HPP_
