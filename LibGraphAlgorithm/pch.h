51085b9c682590621deb3723a3f2cd0cd5e13de2
Sallee <885331635@qq.com>
2024-02-18 14:35:41 +0800

    feat: 创建外部接口项目

--- LibGraphAlgorithm/pch.h
+++ LibGraphAlgorithm/pch.h
@@ -8,7 +8,9 @@
 #define PCH_H
 
 #ifdef _WIN32
-#include "framework.h"
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+// Windows 头文件
+#include <windows.h>
 #endif // WIN32
 
 #endif //PCH_H
